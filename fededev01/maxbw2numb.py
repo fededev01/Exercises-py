ad4b2ab1817bcc89dfc73fdaa219363f239981ac
Federico <45139706+fededev01@users.noreply.github.com>
2020-04-04 11:49:13 +0200

    Update maxbw2numb.py

--- fededev01/maxbw2numb.py
+++ fededev01/maxbw2numb.py
@@ -1,1 +1,10 @@
+fir = int(input("enter your first number\n"))
 
+sec = int(input("enter your second number\n"))
+
+if fir > sec:
+    print("The greather number is:\n")
+    print(fir)
+else:
+    print("The greather number is:\n")
+    print(sec)
