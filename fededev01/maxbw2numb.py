ecd2f7d5b4dc6952a3af26692230cd737e80b971
Federico <45139706+fededev01@users.noreply.github.com>
2020-04-04 11:52:09 +0200

    Update maxbw2numb.py

--- fededev01/maxbw2numb.py
+++ fededev01/maxbw2numb.py
@@ -5,6 +5,8 @@ sec = int(input("enter your second number\n"))
 if fir > sec:
     print("The greather number is:\n")
     print(fir)
+elif fir == sec:
+       print("Your numbers are identical")
 else:
     print("The greather number is:\n")
     print(sec)
