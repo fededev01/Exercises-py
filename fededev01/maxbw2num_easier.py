977353cfa737d4bcefc501f87bd49037ef961815
Federico <45139706+fededev01@users.noreply.github.com>
2020-04-04 12:44:59 +0200

    Create maxbw2num_easier.py

---
+++ fededev01/maxbw2num_easier.py
@@ -0,0 +1,10 @@
+num = [10, 7]
+
+if num[0] > num[1]:
+    print("The greather number is:\n")
+    print(num[0])
+elif num[0] == num[1]:
+       print("Your numbers are identical")
+else:
+    print("The greather number is:\n")
+    print(num[1])
