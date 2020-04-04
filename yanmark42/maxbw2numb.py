60d5c58989bb2b8db00f92733c7a9b05a804b5fd
YanMark42 <63127468+YanMark42@users.noreply.github.com>
2020-04-04 12:12:58 +0200

    Update maxbw2numb.py

--- yanmark42/maxbw2numb.py
+++ yanmark42/maxbw2numb.py
@@ -1,8 +1,31 @@
-#The programs receives in input two integer numbers and then prints the highest
+#Given two numbers the program prints the highest and checks wether the input is a number or not
 
-print('Input 2 numbers\n')
-a = int(input('The first: '))
-b = int(input('The second: '))
-if (a > b): print(a, 'is the highest number')
-elif (b > a): print(b, 'is the highest number')
-else: print ('The numbers are equal')
+i=1
+
+while i==1:
+    print('Input 2 numbers\n')
+
+    a = input('The first: ')
+
+    if a.isalpha(): 
+        print('Only numbers!\n')
+        continue
+
+    b = input('The second: ')
+    if b.isalpha(): 
+        print('Only numbers!\n')
+        continue
+
+    if (a > b): 
+        print(a, 'is the highest number')
+        break
+
+    elif (b > a): 
+        print (b, 'is the highest number')
+        break
+
+    else: 
+        print ('The numbers are equal')
+        break
+    
+i=2
