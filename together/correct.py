656c70a5d07d3223b39d726bd0e8c671d91ec0e2
Federico <45139706+fededev01@users.noreply.github.com>
2020-04-04 16:30:15 +0200

    Create correct.py

---
+++ together/correct.py
@@ -0,0 +1,10 @@
+lista = []
+
+fine_inserimento = False
+while not fine_inserimento:
+  inserimento = input("enter your number\n")
+if inserimento == 'x':
+   fine_inserimento = True
+else:
+ num = int(inserimento)
+lista.append(num)
