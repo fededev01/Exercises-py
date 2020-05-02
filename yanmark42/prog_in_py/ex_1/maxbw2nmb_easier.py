293dc96b765efc35df0b4a06686c2ef4ef9b2d5a
Federico <45139706+fededev01@users.noreply.github.com>
2020-05-02 15:46:48 +0200

    Risolte le issues #15 e la #10
    Il codice è stato scritto da @yanmark42
    Io ho solo spostato i file tra le cartelle
    
    Co-Authored-By: yanmark42 <yanmark42@users.noreply.github.com>

---
+++ yanmark42/prog_in_py/ex_1/maxbw2nmb_easier.py
@@ -0,0 +1,8 @@
+#Given two integer numbers in input prints the greater
+
+print('Input 2 numbers\n')
+a = int(input('The first: '))
+b = int(input('The second: '))
+if (a > b): print(a, 'is the highest number')
+elif (b > a): print(b, 'is the highest number')
+else: print ('The numbers are equal')
