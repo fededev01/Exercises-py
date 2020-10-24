7dfef8e34a62a86c4345525a34212183b9b20cca
Federico <45139706+fededev01@users.noreply.github.com>
2020-10-24 13:19:40 +0200

    Create is_vocal.py

---
+++ fededev01/prog_in_py/ex_3/is_vocal.py
@@ -0,0 +1,6 @@
+vocals = ["a", "e", "i", "o", "u"]
+x = str(input("Insert here your letter: \n"))
+if x in vocals:
+    print("Your letter is a vocal")
+else:
+    print("Your letter isn't a vocal")    
